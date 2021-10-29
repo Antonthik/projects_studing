efd38ec96a8b9fedb2cd990f5aedf1449b785f08
Antonthik <93084395+Antonthik@users.noreply.github.com>
2021-10-29 16:01:29 +0300

    Add files via upload
    task1 create

---
+++ lesson1/Lesson1-task1/Lesson1-task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson1_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Выводим строку на дисплей для пользователя
+            Console.WriteLine("Введите свое имя.");
+            // Читаем имя вводимое пользователем
+            string nameUser = Console.ReadLine();
+            //Определяем текущую дату и время
+            DateTime toDay = DateTime.Now;
+            //Применяем интерполяцию для построения динамической строки (используем - $ перед строкой, и переменные указываем - {})
+            Console.WriteLine($"{nameUser}, привет сегодня {toDay} !"); 
+        }
+    }
+}
